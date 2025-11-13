44d7d0128e9614e425e11fa593f5c1e4a48972b9
Mike Stonoga Machado <mikestonoga@hotmail.com>
2025-11-12 21:12:34 -0300

    fixing cors

--- server/API.WebHost/Settings/CorsSettings.cs
+++ server/API.WebHost/Settings/CorsSettings.cs
@@ -5,7 +5,7 @@ public static class CorsSettings
     public static IServiceCollection RegisterCors(this IServiceCollection services)
     {
         services.AddCors(o => o.AddPolicy("AllowAngular",
-            p => p.WithOrigins("http://localhost:4200")
+            p => p.WithOrigins("http://localhost:4200", "https://fleetmanager.providencesoft.com.br")
                   .AllowAnyHeader()
                   .AllowAnyMethod())
         );
