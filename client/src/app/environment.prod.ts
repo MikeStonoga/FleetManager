943f118cc168fcdb0dd8c48d1750cafe928ae73f
Mike Stonoga Machado <mikestonoga@hotmail.com>
2025-11-12 23:32:55 -0300

    adding docker compose prod

---
+++ client/src/app/environment.prod.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+  production: false,
+  API_URL: 'https://fleetmanagerapi.providencesoft.com.br/api'
+};
--- client/src/app/environment.ts
+++ client/src/app/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_URL: 'https://fleetmanagerapi.providencesoft.com.br/api'
+  API_URL: 'http://localhost:5000/api'
 };
